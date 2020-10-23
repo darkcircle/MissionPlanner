4350804a7e2da17613342d5ae851c897296c36cf
Michael Oborne <mich146@hotmail.com>
2020-10-23 10:03:46 +0800

    xamarin: build fix

--- ExtLibs/Xamarin/Xamarin.Android/BTDevice.cs
+++ ExtLibs/Xamarin/Xamarin.Android/BTDevice.cs
@@ -4,6 +4,26 @@ using Android.Bluetooth;
 using Android.Util;
 using MissionPlanner.ArduPilot;
 
+namespace OSGeo
+{
+    public class GdalConfiguration
+    {
+        public static void ConfigureGdal()
+        {
+        
+        }
+
+        public static void ConfigureOgr()
+        {
+        
+        }
+    }
+}
+namespace OSGeo.GDAL
+{ }
+namespace OSGeo.OGR
+{ }
+
 namespace Xamarin.Droid
 {
     public class BTDevice : IBlueToothDevice
